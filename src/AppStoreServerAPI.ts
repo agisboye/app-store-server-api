e4ae8d5684d1567175183c92b3e52844fe31bd1a
agisboye <august@heegaard.org>
2021-12-11 14:11:09 +0100

    Move decoding into separate file

--- src/AppStoreServerAPI.ts
+++ src/AppStoreServerAPI.ts
@@ -1,4 +1,3 @@
-import { X509Certificate } from "crypto"
 import fetch from "node-fetch"
 import { v4 as uuidv4 } from "uuid"
 import * as jose from "jose"
@@ -12,8 +11,9 @@ import {
   OrderLookupResponse,
   StatusResponse
 } from "./Models"
-import { APPLE_ROOT_CA_G3_FINGERPRINT } from "./AppleRootCertificate"
-import { AppStoreError, CertificateValidationError } from "./Errors"
+
+import { AppStoreError } from "./Errors"
+import { decodeJWS } from "./Decoding"
 
 export class AppStoreServerAPI {
   
@@ -77,15 +77,15 @@ export class AppStoreServerAPI {
   // Decoding helpers
 
   async decodeTransactions(signedTransactions: JWSTransaction[]): Promise<JWSTransactionDecodedPayload[]> {
-    return Promise.all(signedTransactions.map(this.decodeJWS))
+    return Promise.all(signedTransactions.map(decodeJWS))
   }
 
   async decodeTransaction(transaction: JWSTransaction): Promise<JWSTransactionDecodedPayload> {
-    return this.decodeJWS(transaction)
+    return decodeJWS(transaction)
   }
 
   async decodeRenewalInfo(info: JWSRenewalInfo): Promise<JWSRenewalInfoDecodedPayload> {
-    return this.decodeJWS(info)
+    return decodeJWS(info)
   }
 
   /**
@@ -164,61 +164,5 @@ export class AppStoreServerAPI {
     const cutoff = new Date(now.getTime() - headroom * 1000)
 
     return !this.tokenExpiry || this.tokenExpiry < cutoff
-  }
-
-  /**
-   * Decodes and verifies an object signed by the App Store according to JWS.
-   * See: https://developer.apple.com/documentation/appstoreserverapi/jwstransaction
-   */
-  private async decodeJWS(token: string): Promise<any> {
-    // Extracts the key used to sign the JWS from the header of the token
-    const getKey: jose.CompactVerifyGetKey = async (protectedHeader, _token) => {
-      // RC 7515 stipulates that the key used to sign the JWS must be the first in the chain.
-      // https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.6
-
-      // jose will not import the certificate unless it is in a proper PKCS8 format.
-      const certs = protectedHeader.x5c?.map(c => `-----BEGIN CERTIFICATE-----\n${c}\n-----END CERTIFICATE-----`) ?? []
-
-      this.validateCertificates(certs)
-
-      return jose.importX509(certs[0], "ES256")
-    }
-
-    const { payload } = await jose.compactVerify(token, getKey)
-
-    const decoded = new TextDecoder().decode(payload)
-    const json = JSON.parse(decoded)
-
-    return json
-  }
-
-  /**
-   * Validates a certificate chain provided in the x5c field of a decoded header of a JWS.
-   * The certificates must be valid and have come from Apple.
-   * @throws {CertificateValidationError} if any of the validation checks fail
-   */
-  private validateCertificates(certificates: string[]) {
-    if (certificates.length === 0) throw new CertificateValidationError([])
-
-    const x509certs = certificates.map(c => new X509Certificate(c))
-
-    // Check dates
-    const now = new Date()
-    const datesValid = x509certs.every(c => new Date(c.validFrom) < now && now < new Date(c.validTo))
-    if (!datesValid) throw new CertificateValidationError(certificates)
-
-    // Check that each certificate, except for the last, is issued by the subsequent one.
-    if (certificates.length >= 2) {
-      for (let i = 0; i < x509certs.length - 1; i++) {
-        if (x509certs[i].checkIssued(x509certs[i + 1]) === false) {
-          throw new CertificateValidationError(certificates)
-        }
-      }
-    }
-
-    // Ensure that the last certificate in the chain is the expected Apple root CA.
-    if (x509certs[x509certs.length - 1].fingerprint256 !== APPLE_ROOT_CA_G3_FINGERPRINT) {
-      throw new CertificateValidationError(certificates)
-    }
   }
 }
---
+++ src/Decoding.ts
@@ -0,0 +1,60 @@
+import { X509Certificate } from "crypto"
+import * as jose from "jose"
+import { APPLE_ROOT_CA_G3_FINGERPRINT } from "./AppleRootCertificate"
+import { CertificateValidationError } from "./Errors"
+
+/**
+ * Decodes and verifies an object signed by the App Store according to JWS.
+ * See: https://developer.apple.com/documentation/appstoreserverapi/jwstransaction
+ */
+export async function decodeJWS(token: string): Promise<any> {
+  // Extracts the key used to sign the JWS from the header of the token
+  const getKey: jose.CompactVerifyGetKey = async (protectedHeader, _token) => {
+    // RC 7515 stipulates that the key used to sign the JWS must be the first in the chain.
+    // https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.6
+
+    // jose will not import the certificate unless it is in a proper PKCS8 format.
+    const certs = protectedHeader.x5c?.map(c => `-----BEGIN CERTIFICATE-----\n${c}\n-----END CERTIFICATE-----`) ?? []
+
+    validateCertificates(certs)
+
+    return jose.importX509(certs[0], "ES256")
+  }
+
+  const { payload } = await jose.compactVerify(token, getKey)
+
+  const decoded = new TextDecoder().decode(payload)
+  const json = JSON.parse(decoded)
+
+  return json
+}
+
+/**
+ * Validates a certificate chain provided in the x5c field of a decoded header of a JWS.
+ * The certificates must be valid and have come from Apple.
+ * @throws {CertificateValidationError} if any of the validation checks fail
+ */
+function validateCertificates(certificates: string[]) {
+  if (certificates.length === 0) throw new CertificateValidationError([])
+
+  const x509certs = certificates.map(c => new X509Certificate(c))
+
+  // Check dates
+  const now = new Date()
+  const datesValid = x509certs.every(c => new Date(c.validFrom) < now && now < new Date(c.validTo))
+  if (!datesValid) throw new CertificateValidationError(certificates)
+
+  // Check that each certificate, except for the last, is issued by the subsequent one.
+  if (certificates.length >= 2) {
+    for (let i = 0; i < x509certs.length - 1; i++) {
+      if (x509certs[i].checkIssued(x509certs[i + 1]) === false) {
+        throw new CertificateValidationError(certificates)
+      }
+    }
+  }
+
+  // Ensure that the last certificate in the chain is the expected Apple root CA.
+  if (x509certs[x509certs.length - 1].fingerprint256 !== APPLE_ROOT_CA_G3_FINGERPRINT) {
+    throw new CertificateValidationError(certificates)
+  }
+}
