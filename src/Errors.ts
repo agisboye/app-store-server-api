b5c94d4fea52b71ac448790c81032bd74dce0adc
agisboye <august@heegaard.org>
2023-03-20 15:39:00 +0100

    Add isRetryable property to errors

--- src/Errors.ts
+++ src/Errors.ts
@@ -1,9 +1,22 @@
 export class AppStoreError extends Error {
+  
+  // The following errors indicate that the request can be tried again.
+  // See https://developer.apple.com/documentation/appstoreserverapi/error_codes
+  // for a list of all errors.
+  static readonly RETRYABLE_ERRORS = [
+    4040002, // AccountNotFoundRetryableError
+    4040004, // AppNotFoundRetryableError
+    5000001, // GeneralInternalRetryableError
+    4040006 // OriginalTransactionIdNotFoundRetryableError
+  ]
+
   errorCode: number
+  isRetryable: boolean
 
   constructor(errorCode: number, errorMessage: string) {
     super(errorMessage)
     this.errorCode = errorCode
+    this.isRetryable = AppStoreError.RETRYABLE_ERRORS.includes(errorCode)
   }
 }
 
